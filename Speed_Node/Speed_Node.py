1345315d6f3c55d98f30f5117fc7a11a6cdf6ae3
Shreyas Ramakrishna <31136074+Shreyasramakrishna90@users.noreply.github.com>
2020-03-15 18:01:59 -0500

    speed node

---
+++ Speed_Node/Speed_Node.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import base64
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import Float32
+import IO
+import time
+
+
+def Speed():
+    rospy.init_node('Speed_Node', disable_signals=True)
+    pub = rospy.Publisher('Speed', Float32, queue_size=1)
+    rate = rospy.Rate(10) # 10hz
+    try:
+        while not rospy.is_shutdown():
+            speed = IO.speed
+            speed = float("{0:.2f}".format(speed))
+            pub.publish(speed)
+            print("publishing speed %s" %rospy.get_time())
+            rate.sleep()
+    except KeyboardInterrupt:
+        sys.exit()
+
+if __name__ == '__main__':
+    IO.initGPIO(100,0,0)#Initialize the GPIO pins
+    IO.beginGettingSpeed()#Initialize the opto-coupler to get code
+    IO.changeDutyCycle((15, 15))
+    time.sleep(2)
+    Speed()
